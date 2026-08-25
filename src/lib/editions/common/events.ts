55045cdefe58ffc3b68aff11a779c37fcbfa2d4e
Inklay <inklay@outlook.fr>
2026-08-25 16:18:58 +0200

    Added the 2011 and 2012 minecons

--- src/lib/editions/common/events.ts
+++ src/lib/editions/common/events.ts
@@ -2,10 +2,37 @@ import type { Version } from "..";
 
 export const events: Version[] = [
   {
+    title: "Minecon 2011",
+    type: "event",
+    date: "2011-11-18",
+    icon: "common/event_minecon_2011.png",
+    learnMore: "@MINECON_2011",
+    longDescription: [
+      "The first official Minecon event held in 2011 at Las Vegas.",
+      "The event coincided with the release of Minecraft 1.0.",
+      "There were keynotes from Notch and other developers as well as many activities such as a build competition, a costume contest, and more."
+    ]
+  },
+
+  {
+    title: "Minecon 2012",
+    type: "event",
+    date: "2012-11-24",
+    icon: "common/event_minecon_2012.png",
+    learnMore: "@MINECON_2012",
+    longDescription: [
+      "The second official Minecon event held in 2012 at Disney land Paris.",
+      "The Redstone Update was announced during this event",
+      "This event was also lived streamed on the internet, allowing many more people to watch the event.",
+      "There were keynotes from Notch and other developers as well as many activities such as a build competition, a costume contest, and more."
+    ]
+  },
+
+  {
     title: "Game bought by Microsoft",
     type: "event",
     date: "2014-11-06",
-    icon: "/event_microsoft.png",
+    icon: "common/event_microsoft.png",
     learnMore: "https://web.archive.org/web/20140915195135/https://mojang.com/2014/09/yes-were-being-bought-by-microsoft/",
     longDescription: [
       "On November of 2014, all of Mojang was acquired by Microsoft for $2.5 billion. This included Minecraft, of course.",
